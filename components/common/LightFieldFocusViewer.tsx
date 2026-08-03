30b347f52ad23400084b9d98e7ff90677244c585
phila3d <dave@phila3d.com>
2026-08-03 02:31:19 -0400

    make screen 2 similar view to screen 3

--- components/common/LightFieldFocusViewer.tsx
+++ components/common/LightFieldFocusViewer.tsx
@@ -20,7 +20,8 @@ export const LightFieldFocusViewer: FC<LightFieldFocusViewerProps> = ({
 }) => {
   const fov = 75;
   const planeSize = 1;
-  const cameraZ = planeSize / (2 * Math.tan((fov * Math.PI) / 360));
+// Pull camera back slightly (1.05x factor) so the entire frame fits without top/bottom cropping
+const cameraZ = (planeSize / (2 * Math.tan((fov * Math.PI) / 360))) * 1.05;
 
   useEffect(() => {
     return () => disposeTexture();
